03f3715adf9b5bd7586b3d9d75493ba1a2b8c70b
StudentSmerf <272976@student.pwr.edu.pl>
2023-06-06 19:55:29 +0200

    Added ability to close application

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Quit : MonoBehaviour
+{
+    public void OnQuitPressed(){
+        Application.Quit();
+    }
+}
